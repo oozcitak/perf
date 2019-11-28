15d407b1a8df3514e9b996157c44a8951f1ef296
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-11-28 14:38:06 +0300

    Remove node from typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,28 +1,19 @@
-/// <reference types="node" />
+/**
+ * Defines a suite of performance benchmarks.
+ * 
+ * @param title - title of the benchmark suite
+ * @param func - benchmark contents
+ */
+declare var benchmark: ((title: string, func: (() => void)) => void)
 
-declare namespace NodeJS {
-  // Augments `global` object when node.d.ts is loaded
-  interface Global extends PerfGlobals { }
-}
+/**
+ * Defines a benchmark scenario.
+ * 
+ * @param title - title of the scenario
+ * @param count - number of times to run this scenario, defaults to `1000`
+ * @param func - benchmark contents
+ */
+declare var scenario: ((title: string, count: number | (() => void), func?: (() => void)) => void)
 
-interface PerfGlobals {
- /**
-  * Defines a suite of performance benchmarks.
-  * 
-  * @param title - title of the benchmark suite
-  * @param func - benchmark contents
-  */
- benchmark: ((title: string, func: (() => void)) => void)
- 
- /**
-  * Defines a benchmark scenario.
-  * 
-  * @param title - title of the scenario
-  * @param count - number of times to run this scenario, defaults to `1000`
-  * @param func - benchmark contents
-  */
- scenario: ((title: string, count: number | (() => void), func?: (() => void)) => void)
-
-  _currentBenchmark: string
-  _benchmarkResults: { [key: string]: { [key: string]: number } }
-}
+declare var _currentBenchmark: string
+declare var _benchmarkResults: { [key: string]: { [key: string]: number } }
